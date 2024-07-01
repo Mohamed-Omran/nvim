467d62b20897410d1dcbb81278bd87cf304c592b
Mohamed Omran <mohomr6q@pave360-gpu2.sje.mentorg.com>
2024-07-01 18:07:03 +0000

    adding prep_cpoy remap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,3 +5,27 @@
 local keymap = vim.keymap -- for conciseness
 
 keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
+
+-- local mouse = vim.opt.mouse:get() -- Get the current mouse setting
+local mouse = 'a'
+
+local function prep_copy()
+  -- Toggle the mouse setting
+  if mouse == 'a' then
+    vim.cmd('set mouse=')
+    mouse = ''
+  else
+    vim.cmd('set mouse=a')
+    mouse = 'a'
+  end
+  vim.cmd('set number!')
+  vim.cmd('set relativenumber!')
+  vim.cmd('IBLToggle')
+  vim.cmd('Gitsigns toggle_signs')
+  vim.cmd('lua LazyVim.toggle.diagnostics()')
+  vim.cmd('Neotree toggle')
+
+end
+
+-- Create the keymap to trigger the function
+vim.keymap.set('n', '<leader>uc', prep_copy, { desc = 'Toggle settings: mouse, Neotree, line numbers, indent blank line, and gitsigns' })
